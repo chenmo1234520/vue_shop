1fceb9817ab3852432f55c7e6c7481fe967605c9
陈泽宇 <9454194+chenmo1234520@user.noreply.gitee.com>
2021-08-06 12:18:53 +0800

    完成了登录功能

---
+++ src/components/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    主页
+    <el-button type="info" @click="cleartoken">退出</el-button>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    cleartoken () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style lang="less" scoped></style>
